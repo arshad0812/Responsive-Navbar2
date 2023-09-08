8434204d2c18016d71cbcaf9f130ac8fca9f0258
unknown <adarshadaashish@gmail.com>
2023-09-08 11:14:53 +0530

    Intial commit

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const menutoggle=document.querySelector('.toggle');
+const showcase=document.querySelector('.showcase');
+
+menutoggle.addEventListener('click', ()=>{
+    menutoggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+}
+)
